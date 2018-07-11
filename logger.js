729c04d21e80f2e6a703998b0fd0e7a7f0203580
lucas Borella <blackbox.software@gmail.com>
2018-07-10 21:52:29 -0300

    Fix ...

--- logger.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const moment = require('moment');
-
-var Logger = exports.Logger = {};
-
-let wstream = fs.createWriteStream('myOutput.txt');
-  wstream.write(new Date )
-  wstream.write('message');
-  wstream.end();
