51e4658ee457eb7e82710386f38837b51749e48a
lucas Borella <blackbox.software@gmail.com>
2018-07-30 09:31:53 -0300

    Lesson 5

--- lesson_5/app/routes/users.js
+++ lesson_5/app/routes/users.js
@@ -1,30 +1,38 @@
-var express = require('express');
-var router = express.Router();
-var User = require('../models/User');
+const express = require('express');
+const router = express.Router();
+const expressJoiMiddleware = require('express-joi-middleware');
+const Joi = require('joi');
 
-/* GET users listing. */
-router.get('/create', function(req, res) {
-  // create a new user called Jhon Doe
-  var jDoe = new User({
-    name: 'Jhon Doe',
-    username: 'jdoe',
-    password: 'password' 
-  });
+const userCreateSchema = {
+  body: {
+    name: Joi.string().required(),
+    username: Joi.string().email().required(),
+    password: Joi.string().strip().required(),
+    confirm: Joi.any().valid(Joi.ref('password')).required().options({ language: { any: { allowOnly: 'passwords and confirmation must be equals.' } } }),
+  },
+};
 
-  jDoe.save(function(err) {
-    if (err) throw err;
-  
-    res.send('User saved successfully!');
-  });
-  
-});
+const userUpdateSchema = {
+  body: {
+    name: Joi.string().required(),
+    username: Joi.string().email().required()
+  },
+};
 
-router.get('/list', function(req, res) {
-  // create a new user called Jhon Doe
-  User.find({}, function(err, users) {
-      res.json({ users: users });
-  });
-  
-});
+const options = {
+  wantResponse: true,
+};
+
+const userController = require('../controllers/user.controller');
+
+router.post('/', expressJoiMiddleware(userCreateSchema, options), userController.create);
+
+router.get('/list', userController.findAll);
+
+router.get('/:userId', userController.findOne);
+
+router.put('/:userId', expressJoiMiddleware(userUpdateSchema, options), userController.update);
+
+router.delete('/:userId', userController.delete);
 
 module.exports = router;
