0b5d6dbd2c4a799cb22789e68e8be93878d22eb6
Lucio De Simone <38821051+LucioDeSimone@users.noreply.github.com>
2019-05-24 17:00:22 -0300

    exercise 2 done!

---
+++ exercises/chapter_2/app/app.js
@@ -0,0 +1,24 @@
+const process = require("process");
+const minimist = require("minimist");
+
+let args = minimist(process.argv.slice(2), {
+  alias: {
+    msg: "message"
+  }
+});
+
+const logger = function(msg) {
+  console.log(msg.concat(` --> ${new Date().toDateString()}`));
+};
+
+if (!!!args.msg) {
+  console.log("usage: [--msg=<message>]");
+} else {
+  if (args.msg === true) {
+    console.log("should enter a message after --msg option...");
+  } else {
+    for (string of args.message.split(" ")) {
+      logger(string);
+    }
+  }
+}
