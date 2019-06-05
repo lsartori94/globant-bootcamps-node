641ab98f1ba826898df2056925f9713e12eb059b
Lucia Benito <lu.benitoc@gmail.com>
2019-06-05 15:59:31 -0300

    Adder missing mocks for profiles and roles

---
+++ exercises/chapter_5/src/test-helpers/roles.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/****************************
+*        ROLES MOCK         *
+***************************/
+const ALL_ROLES = [
+    {
+        id: 1,
+        name: 'A role',
+        createdAt: '2019-02-20T18:49:27'
+    },
+    {
+        id: 2,
+        name: 'Another role',
+        createdAt: '2019-02-20T18:49:27'
+    },
+    {
+        id: 3,
+        name: 'Any other role',
+        createdAt: '2019-02-20T18:49:27'
+    }
+];
+
+module.exports = {
+    ALL_ROLES: ALL_ROLES
+};
