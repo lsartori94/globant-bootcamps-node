e1cbfccac2c251e40d2ee40ab3ed437fff1cf951
Lucia Benito <lu.benitoc@gmail.com>
2019-06-08 19:47:11 -0300

    Changed mysql credential

--- exercises/chapter_7/src/api-server/config/config.js
+++ exercises/chapter_7/src/api-server/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
-    username: "",
-    password: "",
+    username: "root",
+    password: "root",
     database: "bootcamp",
     host: "127.0.0.1",
     dialect: "mysql",
