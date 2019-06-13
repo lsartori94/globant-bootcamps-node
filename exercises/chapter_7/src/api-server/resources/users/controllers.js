3abea64f5a943b3848d5d9b62c83f96346abf0e5
javier <javiercaraballo97@gmail.com>
2019-06-13 10:57:00 -0300

    commit -am probando

--- exercises/chapter_7/src/api-server/resources/users/controllers.js
+++ exercises/chapter_7/src/api-server/resources/users/controllers.js
@@ -2,7 +2,7 @@
 'use strict';
 
 
-const models=require('../../models');
+const models = require('../../models');
 const _ = require('lodash');
 const actions = require('./actions');
 // const userMock = require('../../../test-helpers/users'); 
@@ -11,9 +11,9 @@ module.exports = {
     v1: { // Initial version
         getAll: getAll,
         getOneByid: getOneByid,
-        createUser:createUser,
-        deleteUser:deleteUser,
-        updateUser:updateUser,
+        createUser: createUser,
+        deleteUser: deleteUser,
+        updateUser: updateUser,
 
 
     }
@@ -26,64 +26,64 @@ module.exports = {
  * @param {Object} req - http.ServerRequest
  * @param {Object} res - http.ServerResponse
  */
- async function getAll(req, res) {
+async function getAll(req, res) {
     const users = await models.User.findAll({
-        attributes: [ 'id','name','lastname', 'email'],
+        attributes: ['id', 'name', 'lastname', 'email'],
         order: [
             ['id', 'DESC']
         ]
     });
-console.log(users[0]);
+    console.log(users[0]);
     res.json({ users });
 
 }
 
 
- async function getOneByid(req,res){
+async function getOneByid(req, res) {
 
-    const {id} = req.params;
+    const { id } = req.params;
     const user = await models.User.findOne({
         where: { id },
-        attributes: [ 'id','name','lastname', 'email'],
+        attributes: ['id', 'name', 'lastname', 'email'],
     });
-   // console.log(user,'lalalalla');
+    // console.log(user,'lalalalla');
     // const a=1;
     // debugger;
     if (user == null) {
         res.status(404).send('No se encuentra el id buscado');
 
-    }else {
+    } else {
         res.json(user);
 
     }
-   
 
-   // console.log('Ace deberiamos buscar uno por id',id);
-   
+
+    // console.log('Ace deberiamos buscar uno por id',id);
+
+
+    /*    const { id } = req.params;
     
-/*    const { id } = req.params;
-
-    const user = await .findOne({
-        where: { id },
-        attributes: ['id', 'projectid', 'name', 'done']
-    });
-    res.json(task);
-
-}*/
+        const user = await .findOne({
+            where: { id },
+            attributes: ['id', 'projectid', 'name', 'done']
+        });
+        res.json(task);
     
+    }*/
+
 }
 
-async function createUser(req,res){
-    const { name, password, lastname,email } = req.body;
-       
-    
+async function createUser(req, res) {
+    const { name, password, lastname, email } = req.body;
+
+
     const newUser = await models.User.create({
         name,
         password,
         lastname,
         email
     }, {
-            fields: ['name', 'password', 'lastname','email']// que fields voy a pasar cuando creo un nuevo dato
+            fields: ['name', 'password', 'lastname', 'email']// que fields voy a pasar cuando creo un nuevo dato
         });
 
 
@@ -91,35 +91,35 @@ async function createUser(req,res){
 
 }
 // ojo delete tira user deleted si no encuentra id
- async function deleteUser(req, res) {
+async function deleteUser(req, res) {
     const { id } = req.params;
 
-   const deleted= await models.User.destroy({
+    const deleted = await models.User.destroy({
         where: {
             id
         }
     })
     //console.log(deleted,'a');
-    if (deleted != 0){
-    res.json({ message: `User with id: ${id}, was deleted` })
-    }else{
-    res.json({message:`User with id: ${id} no exists`})
+    if (deleted != 0) {
+        res.json({ message: `User with id: ${id}, was deleted` })
+    } else {
+        res.json({ message: `User with id: ${id} no exists` })
     }
 }
 
 async function updateUser(req, res) {
 
     const { id } = req.params;
-    const { name, lastname, email,password} = req.body;
+    const { name, lastname, email, password } = req.body;
 
     const user = await models.User.findOne({
-        attributes: [ 'id','name','lastname', 'email','password'],
+        attributes: ['id', 'name', 'lastname', 'email', 'password'],
         where: { id }
     })
-// aca tendria que ver que es lo que se actualiza y que es lo que pasan 
-// preguntar diferencia entre put y patch no me acuerdo, o actualizo todo
-// se podria hacer un patch solo para password 
-    const updatedTask = await models.User.update({
+    // aca tendria que ver que es lo que se actualiza y que es lo que pasan 
+    // preguntar diferencia entre put y patch no me acuerdo, o actualizo todo
+    // se podria hacer un patch solo para password 
+    const updatedUser = await models.User.update({
         name,
         lastname,
         email,
@@ -129,10 +129,17 @@ async function updateUser(req, res) {
         {
             where: { id }
         })
-    res.json({
-        message: 'Task updated',
-        updatedTask
-    })
+    if (updatedUser == 1) {
+        res.json({
+            message: 'User updated successfully',
+
+        });
+    }else {
+        res.status(400).json({
+            message: 'Some error occurred',
+
+        });
+    }
 
 }
 
